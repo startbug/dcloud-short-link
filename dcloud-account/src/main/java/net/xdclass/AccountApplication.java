35f7fb3d397b403250cbb97ceb30165df0d6c1f2
startbug <1285226024@qq.com>
2023-04-18 23:36:11 +0800

    服务注册发现

---
+++ dcloud-account/src/main/java/net/xdclass/AccountApplication.java
@@ -0,0 +1,26 @@
+package net.xdclass;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author starbug
+ * @Description
+ * @Datetime 2023/4/18 23:27
+ */
+@MapperScan("net.xdclass.mapper")
+@EnableTransactionManagement
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
